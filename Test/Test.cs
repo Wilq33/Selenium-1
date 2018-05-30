ef824924ef27c785bf106299ab48ccb6ecd72a72
Wilk Bartlomiej <wilkbartlomiej26@gmail.com>
2018-05-30 14:58:12 +0200

    Test 1st part
    Putting everything together.

---
+++ Test/Test.cs
@@ -0,0 +1,70 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
+
+class Test
+{
+    static void Main()
+    {
+        string url = "http://demoqa.com/selectable/";
+
+        //Selectors
+        string nameSelector = "";
+        string idSelector = "nav_menu-2";
+        string cssPathSelector = "#post-41 > header > h1";
+        string xPathSelector = "";
+
+        IWebDriver driver = new ChromeDriver();
+
+
+        //Elements
+        //IWebElement elementName = driver.FindElement(By.Name(nameSelector));
+        IWebElement elementId;
+        IWebElement elementCssPath = driver.FindElement(By.CssSelector(cssPathSelector));
+        //IWebElement elementXPath = driver.FindElement(By.XPath(xPathSelector));
+
+        driver.Navigate().GoToUrl(url);
+
+        try
+        {
+            elementId = driver.FindElement(By.Id(idSelector));
+
+            if (elementId.Displayed)
+            {
+                GreenMessage("I can see the ID element");
+                Debug.WriteLine(elementId.Text);
+            }
+           
+        }
+        catch (NoSuchElementException)
+        {
+            GreenMessage("I don't see the ID element");
+        }
+
+
+
+
+    }
+
+    
+    
+
+    static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+
+
+}
+
